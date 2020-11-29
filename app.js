5f032e9cab9777b9f0b2563bea636e932e2da3e3
Satyam Gaur <sgsatyamg@gmail.com>
2020-11-29 20:02:32 +0530

    Completed backend for playlist download

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
-// const express = require('express');
-// const app = express();
 const { Builder, By, Key, until } = require('selenium-webdriver');
-// const webdriver = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 const chromedriver = require('chromedriver');
 
@@ -11,41 +8,50 @@ let links = [];
 let driver = new Builder().forBrowser('chrome').build();
 const actions = driver.actions();
 
-async function collectLinks() {
-    await driver.get('https://www.youtube.com/playlist?list=PL38QlIeqCY_ZPvQI_CekD7OGxtvOuY2bS');
-    let elements = await (await driver.findElements(By.className('yt-simple-endpoint style-scope ytd-playlist-video-renderer'))).forEach(element => {
-        let link = element.getAttribute('href');
-        links.push(link);
-    })
+async function collectLinks(playlistLink) {
+  await driver.get(playlistLink);
+  let elements = await driver.findElements(By.className('yt-simple-endpoint style-scope ytd-playlist-video-renderer'));
+  for (let i = 0; i < elements.length; i++) {
+    let link = await elements[i].getAttribute('href');
+    links.push(link);
+    if (i == elements.length - 1) {
+      for (let j = 0; j < links.length; j++) {
+        console.log(j);
+        await driver.get('https://www.y2mate.com/en63');
+        let url = await driver.findElement(By.id('txt-url'));
+        // console.log(url);
+        await url.sendKeys(links[j], Key.RETURN);
+        await driver.wait(until.elementLocated(By.className('btn btn-success')), 10000);
+        let downloadButton = await driver.findElements(By.className('btn btn-success'));
+        // console.log(downloadButton);
+        await downloadButton[0].click();
+        console.log('clicked');
+
+        await driver.wait(until.elementLocated(By.className('btn btn-success btn-file')), 10000);
+        let downloadButton2 = await driver.findElements(By.className('btn btn-success btn-file'));
+        // console.log(downloadButton2);
+        await downloadButton2[0].click();
+        console.log('clicked');
+
+        console.log('Waiting for download');
+        await driver.sleep(2000);
+        console.log('Wait finished');
+        
+        let windowHandles = await driver.getAllWindowHandles();
+        console.log(windowHandles);
+
+        if (windowHandles.length == 2) {
+          console.log('before switching');
+          await driver.switchTo().window(windowHandles[1]);
+          console.log('after switching');
+          await driver.close();
+          console.log('closed');
+          await driver.switchTo().window(windowHandles[0]);
+          console.log('Final switch');
+        }
+      }
+    }
+  }
 };
 
-async function downloadVideo(link) {
-    await driver.get('https://en.savefrom.net/1-youtube-video-downloader-3/');
-    await driver.findElement(By.name('sf_url')).sendKeys(link, Key.RETURN);
-    
-    setTimeout(async function() {
-        await (await driver.findElements(By.className('link link-download no-downloadable subname ga_track_events download-icon'))).forEach(async function(element) {
-            console.log("Clicking!");
-            await actions
-                .keyDown(Key.ALT)
-                .click(element)
-                .keyUp(Key.ALT)
-                .perform();
-        })
-    }, 3000);
-};
-
-downloadVideo('https://www.youtube.com/watch?v=nkxMZbHnWmU');
-
-// app.get("/", function (req, res) {
-//     example();
-//     res.render("index.ejs");
-// })
-
-// app.listen(3000, function (err) {
-//     if (err) {
-//         console.log(err);
-//     } else {
-//         console.log("Server has been started on port 3000!");
-//     }
-// })
+collectLinks('https://www.youtube.com/playlist?list=PL38QlIeqCY_YThz55wOBYdo6UlElN7BFF');
